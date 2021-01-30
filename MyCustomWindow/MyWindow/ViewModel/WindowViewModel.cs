d612e14ba0071880cc8297200336c57d1c7744d9
Luke <lukeom6@web.de>
2021-01-30 14:03:47 +0100

    WindowViewModel Update

--- MyCustomWindow/MyWindow/ViewModel/WindowViewModel.cs
+++ MyCustomWindow/MyWindow/ViewModel/WindowViewModel.cs
@@ -21,10 +21,22 @@ namespace MyCustomWindow.MyWindow.ViewModel
 
         private readonly Window mWindow;
 
+        private readonly int iResizeBorder = 6;
+
+        private readonly int iOuterMargin = 10;
+
+
         #endregion
 
         #region Public Member
 
+        public Thickness ResizeBorderThickness { get => new Thickness(iResizeBorder + iOuterMargin); }
+
+        public Thickness OuterMarginThickness { get => new Thickness(iOuterMargin); }
+
+        public Thickness InnerContent { get => new Thickness(iResizeBorder); }
+
+        public int TitleHeight { get; set; } = 40;
 
         #endregion
 
